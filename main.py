1639cc5e823c370b596f00a0aac47953c347ea0c
waterdancekyk <iaros.rijkow2015@mail.ru>
2024-10-26 10:31:14 +0300

    12312312

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import math
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtCore import QRect
@@ -9,7 +10,7 @@ class SquareApp(QWidget):
         super().__init__()
 
         # Инициализация переменных
-        self.squares = []  # список квадратов с их координатами и цветами
+        self.squares = []  # Список квадратов с их координатами и цветами
         self.success_count = 0
         self.fail_count = 0
 
@@ -38,13 +39,14 @@ class SquareApp(QWidget):
 
     def add_square(self):
         size = 50
+        radius = 10
         max_attempts = 10
         for _ in range(max_attempts):
             # Генерация случайной позиции
             x = random.randint(0, self.width() - size)
             y = random.randint(0, self.height() - size)
             new_square = QRect(x, y, size, size)
-            
+
             # Проверка на пересечение
             if not any(new_square.intersects(square[0]) for square in self.squares):
                 color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
@@ -52,11 +54,23 @@ class SquareApp(QWidget):
                 self.success_count += 1
                 break
             else:
-                for i, (square, color) in enumerate(self.squares):
-                    self.squares[i] = (square, QColor(255, 0, 0))  # окраска в красный при пересечении
                 self.fail_count += 1
+                self.set_squares_red(new_square, radius)
         self.update_stats()
         self.update()
+
+    def set_squares_red(self, new_square, radius):
+        # Изменяем цвет только тех квадратов, которые не находятся в радиусе 10 пикселей от нового квадрата
+        for i, (square, color) in enumerate(self.squares):
+            if new_square.intersects(square) and not self.is_within_radius(new_square, square, radius):
+                self.squares[i] = (square, QColor(255, 0, 0))  # окраска в красный при пересечении
+
+    def is_within_radius(self, square1, square2, radius):
+        # Проверка, чтобы квадрат находился в радиусе
+        center1 = square1.center()
+        center2 = square2.center()
+        distance = math.sqrt((center1.x() - center2.x()) ** 2 + (center1.y() - center2.y()) ** 2)
+        return distance <= radius
 
     def remove_square(self):
         if self.squares:
